7259a3a07f72b6c6cf15d4cc8fcbe32197b6c327
bcs <bcs@coever.co.kr>
2022-12-22 16:43:24 +0900

    초기 커밋.

---
+++ RingTest/RingTest/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RingTest
+{
+    public partial class MainPage : ContentPage
+    {
+        PlaySoundClass test;
+
+        public MainPage()
+        {
+            InitializeComponent();
+            test = new PlaySoundClass();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            test.PlaySound();
+        }
+
+        private void Button_Clicked_1(object sender, EventArgs e)
+        {
+            test.StopSound();
+        }
+    }
+}
---
+++ RingTest/RingTest/PlaySoundClass.cs
@@ -0,0 +1,51 @@
+﻿using Plugin.SimpleAudioPlayer;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using System.Text;
+
+namespace RingTest
+{
+    public class PlaySoundClass
+    {
+        ISimpleAudioPlayer soundBreath = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+        ISimpleAudioPlayer soundHold = Plugin.SimpleAudioPlayer.CrossSimpleAudioPlayer.Current;
+
+        public PlaySoundClass()
+        {
+            try
+            {
+                Stream stream;
+                stream = GetDeviceInfo.GetStream("Sound", "Kalimba.mp3");
+                soundBreath.Load(stream);
+            }
+            catch (Exception ex)
+            {
+
+            }
+   
+        }
+
+        public void PlaySound()
+        {
+            soundBreath.Play();
+        }
+        public void StopSound()
+        {
+            soundHold.Play();
+        }
+    }
+
+    public static class GetDeviceInfo
+    {
+        public static Stream GetStream(string FolderName, string fileName)
+        {
+            TypeInfo pageInfo = typeof(GetDeviceInfo).GetTypeInfo();
+            Assembly assembly = pageInfo.Assembly;
+            string[] resourceNames = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+            Stream abc = assembly.GetManifestResourceStream($"{resourceNames[0]}");
+            return abc;
+        } 
+    }
+}
